f6b3150001a950069c6f502a9867f999cdf1bdb6
Cook1619 <cook6348@gmail.com>
2020-09-01 14:45:42 -0500

    Starting app with testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, cleanup, screen } from "@testing-library/react";
+import App, { list } from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// CLeans up to avoid memory leaks
+afterEach(cleanup);
+
+test("renders a heading", () => {
+  render(<App />);
+  screen.getByText("My Hacker Stories");
 });
+
+for (let i = 0; i < list.length; i++) {
+  test("list should have properties ( title, url, author, num_comments,points,objectID)", () => {
+    expect(list[i]).toHaveProperty("title");
+    expect(list[i]).toHaveProperty("url");
+    expect(list[i]).toHaveProperty("author");
+    expect(list[i]).toHaveProperty("num_comments");
+    expect(list[i]).toHaveProperty("points");
+    expect(list[i]).toHaveProperty("objectID");
+  });
+}
